1b432ee638ce0aff433cba707e8ff48300dab5c4
ayush-921 <gupta.ayush16289@gmail.com>
2022-03-18 20:21:55 +0530

    update 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
+import './screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,17 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'Zoom Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const Text("Hello World"),
+      routes: {
+        '/login': ((context) => const LoginScreen()),
+      },
+      home: const LoginScreen(),
     );
   }
 }
-
