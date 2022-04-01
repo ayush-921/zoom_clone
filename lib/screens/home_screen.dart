c49dc07476b5a38e7a7d0f923ea3ef662e98511d
ayush-921 <gupta.ayush16289@gmail.com>
2022-04-01 22:45:35 +0530

    Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
+import 'package:zoom_clone/widgets/home_meet_btn.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +12,97 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    int _page = 0; //selected item index
+    onPageChanged(int page) {
+      setState(() {
+        _page = page;
+      });
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: backgroundColor,
+        title: const Text('Meet & Chat'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              HomeMeetBtn(
+                onPressed: () {},
+                text: 'New Meeting',
+                clr: Colors.deepOrangeAccent,
+                icon: Icons.videocam,
+              ),
+              HomeMeetBtn(
+                  onPressed: () {},
+                  text: 'Join',
+                  icon: Icons.add_box_rounded,
+                  clr: buttonColor),
+              HomeMeetBtn(
+                onPressed: () {},
+                text: 'Schedule',
+                icon: Icons.calendar_today_rounded,
+                clr: buttonColor,
+              ),
+              HomeMeetBtn(
+                onPressed: () {},
+                text: 'Share Screen',
+                icon: Icons.screen_share_rounded,
+                clr: buttonColor,
+              )
+            ],
+          ),
+          const Expanded(
+            child: Center(
+                child: Text(
+              'Create/Join Meeting with Just One Tap',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            )),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: footerColor,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          onTap: onPageChanged,
+          type: BottomNavigationBarType.fixed,
+          unselectedFontSize: 14,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.chat_bubble_outline,
+              ),
+              label: 'Meet & Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.schedule_rounded,
+              ),
+              label: 'Meetings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.contacts_outlined,
+              ),
+              label: 'Contacts',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_outlined),
+              label: 'Settings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings_applications_outlined),
+              label: 'Meet & Chat',
+            ),
+          ]),
+    );
   }
 }
